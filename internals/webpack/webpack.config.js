51f7bca3522d72fce8c2becb911b72f6ae4e27e8
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-11 14:31:28 -0300

    Update webpack.config.js

--- internals/webpack/webpack.config.js
+++ internals/webpack/webpack.config.js
@@ -1,8 +1,8 @@
 var webpack = require('webpack'); // eslint-disable-line
 
 var env = process.env.NODE_ENV;   // eslint-disable-line
-var filename = 'ethjs-contract';      // eslint-disable-line
-var library = 'EthContract';          // eslint-disable-line
+var filename = 'puffsjs-contract';      // eslint-disable-line
+var library = 'PuffsContract';          // eslint-disable-line
 var config = {                    // eslint-disable-line
   module: {
     loaders: [
